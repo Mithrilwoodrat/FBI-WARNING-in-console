ec2dc33a88ae3e5dbe151c92f35a08c826f63a91
mithrilwoodrat <mithrilwoodrat@gmail.com>
2015-10-11 23:09:30 +0800

    initail commit

---
+++ fbi_warning.sh
@@ -0,0 +1,8 @@
+echo "$(tput cuf 10) $(tput setab 1)FBI WARNING$(tput sgr 0)"
+echo ""
+echo "Federal Law provides severe civil and criminal penalties for
+the unauthorized reproduction, distribution, or exhibition of
+copyrighted motion pictures (Title 17, United States Code,
+Sections 501 and 508). The Federal Bureau of Investigation
+investigates allegations of criminal copyright infringement"
+echo "$(tput cuf 5) (Title 17, United States Code, Section 506)."
