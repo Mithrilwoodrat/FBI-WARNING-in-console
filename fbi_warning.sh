989d6e725cfa3c8541ac485e30a69598571b971c
Danwand <45515141+DanBrown47@users.noreply.github.com>
2020-05-05 14:04:59 +0530

    Updated to more less naive version

--- fbi_warning.sh
+++ fbi_warning.sh
@@ -1,8 +1,12 @@
-echo "$(tput cuf 10) $(tput setab 1)FBI WARNING$(tput sgr 0)"
-echo ""
-echo "Federal Law provides severe civil and criminal penalties for
+# author :  DanBrown47
+# author :  Mithrilwoodrat
+
+echo "$(tput cuf 20) $(tput setab 1)FBI WARNING$(tput sgr 0)"
+# tput set at 20 to adjust basic layout of terminal (Gnome and xterm tested) to be at center 
+echo "Federal Law provides severe civil and criminal penalties for 
 the unauthorized reproduction, distribution, or exhibition of
-copyrighted motion pictures (Title 17, United States Code,
-Sections 501 and 508). The Federal Bureau of Investigation
-investigates allegations of criminal copyright infringement"
+copyrighted software (Title 18, United States Code,
+Sections 502 and 509). The Federal Bureau of Investigation
+investigates allegations of criminal copyright infringement" | pv -qL 50
+
 echo "$(tput cuf 5) (Title 17, United States Code, Section 506)."
